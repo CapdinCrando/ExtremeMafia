58232110f2f62d150e195a0d04acceed8b9edeaf
Tristan Jay <capdincrando@gmail.com>
2019-12-01 15:22:16 -0600

    Added default server files

--- gameView.lua
+++ gameView.lua
@@ -37,8 +37,6 @@ function gameView:hide( event )
  
     if ( phase == "will" ) then
 		-- Code here runs when the scene is on screen (but is about to go off screen)
-		self.un.text = ""
-		self.pw.text = ""
  
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
