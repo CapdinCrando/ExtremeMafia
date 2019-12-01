a5520094e4ea81909a63273bb0d12f4a2eac920f
Tristan Jay <capdincrando@gmail.com>
2019-12-01 16:13:05 -0600

    Finished cleaning and commenting server

--- Server.lua
+++ Server.lua
@@ -1,37 +1,44 @@
 --Project Name: Extreme Mafia
 --Authors: Tristan Jay, Max Stevenson, Jesse Wood
 --Class: CS 371
---Instructor: ?
+--Instructor: Dr. Haeyong Chung
 --Due Date: December 1, 2019
 --File Name: Server.lua
---Description:
+--Description:	Class used to simulate a server enviroment. This class is
+--				for demonstration and would not exist in the actual app.
 
+--Libraries
 local FileUtility = require("FileUtility")
 
+--Filenames for server saves
 local gameSave = "Games.dat"
 local accountSave = "Accounts.dat"
 local tokenSave = "Tokens.dat"
 
+--Create server object
 Server = {}
 
+--Load server states from file
 Server.games = FileUtility.loadTable(gameSave)
-
 Server.accounts = FileUtility.loadTable(accountSave)
-
 Server.tokens = FileUtility.loadTable(tokenSave)
 
+--Saves server game states
 function Server.saveGames()
 	FileUtility.saveTable(Server.games, gameSave)
 end
 
+--Saves server account states
 function Server.saveAccount()
 	FileUtility.saveTable(Server.accounts, accountSave)
 end
 
+--Saves server token states
 function Server.saveTokens()
 	FileUtility.saveTable(Server.tokens, tokenSave)
 end
 
+--Creates a new token for the given user
 function Server.createToken(username)
 	local t = os.date('*t')
 	local time = os.time(t)
@@ -53,7 +60,8 @@ function Server.createToken(username)
 	return uuid
 end
 
---Account functions
+--Adds a new account to the server and returns a new token
+--Returns nil if account cannot be created
 function Server.addAccount(username, password, displayName)
 	if(username ~= nil) then
 		if(password ~= nil) then
@@ -73,6 +81,7 @@ function Server.addAccount(username, password, displayName)
 	return nil
 end
 
+--Removes the given token
 function Server.invalidate(token)
 	local t = Server.tokens[token]
 	if(t ~= nil) then
@@ -83,6 +92,8 @@ function Server.invalidate(token)
 	end
 end
 
+--If username exists and password is correct, returns a new token
+--Returns nil if account information is incorrect
 function Server.login(username, password)
 	local account = Server.accounts[username]
 	if(account ~= nil) then
@@ -96,6 +107,9 @@ function Server.login(username, password)
 	return nil
 end
 
+--Checks to see if the given token is valid
+--If yes, refresh end time and return true
+--If no, return false
 function Server.validate(token)
 	local t = Server.tokens[token]
 	if(t ~= nil) then
@@ -110,6 +124,8 @@ function Server.validate(token)
 	return false
 end
 
+--Gets the account data for the account tied to the given token
+--Returns nil if token is invalid
 function Server.getAccountData(token)
 	if(Server.validate(token)) then
 		local account = Server.accounts[Server.tokens[token].username]
@@ -123,6 +139,8 @@ function Server.getAccountData(token)
 	return nil
 end
 
+--Returns the game data of the game tied to the token's user
+--Returns nil if token is invalid
 function Server.getGameData(token)
 	if(Server.validate(token)) then
 		return Server.games[Server.accounts[Server.tokens[token].username].currentGame]
@@ -130,6 +148,8 @@ function Server.getGameData(token)
 	return nil
 end
 
+--Creates a new game from the given parameters
+--Returns the gameCode
 function Server.createGame(token, t, gameSettings)
 	local game = {
 		title = t,
@@ -148,13 +168,15 @@ function Server.createGame(token, t, gameSettings)
 	if(needed == percentage) then
 		needed = needed + 1
 	end
+	local gameCode = "DHZJ" --Normally random generated
 	game.neededVotes = needed
-	Server.games["DHZJ"] = game
+	Server.games[gameCode] = game
 	Server.saveGames()
 	Server.saveAccount()
-	return "DHZJ" --Normally random generated
+	return gameCode
 end
 
+--Ends the game and declares the winning party given
 function Server.winGame(game, winner)
 	game.winner = winner
 	timer.cancel(game.timer)
@@ -181,6 +203,41 @@ function Server.winGame(game, winner)
 	Server.saveAccount()
 end
 
+--Checks for a potential win condition
+function Server.checkWin(game)
+	--Check for win condition
+	local aliveMafia = 0
+	local aliveCitizens = 0
+	local aliveDoctors = 0
+	for k,v in pairs(game.players) do
+		if(v.role == "mafia") then
+			aliveMafia = aliveMafia + 1
+		else
+			aliveCitizens = aliveCitizens + 1
+			if(v.role == "doctor") then
+				aliveDoctors = aliveDoctors + 1
+			end
+		end
+	end
+
+	if(aliveMafia == 0) then
+		Server.winGame(game, "citizens")
+		return true
+	else
+		if(citizens == 0) then
+			Server.winGame(game, "mafia")
+		return true
+		elseif(citizens == 1) then
+			if(aliveDoctors == 1) then
+				Server.winGame(game, "mafia")
+		return true
+			end
+		end
+	end
+	return false
+end
+
+--Ends the current game round
 function Server.endRound(game)
 	--Check and kill votee
 	local votes = {}
@@ -203,32 +260,7 @@ function Server.endRound(game)
 		end
 	end
 
-	--Check for win condition
-	local aliveMafia = 0
-	local aliveCitizens = 0
-	local aliveDoctors = 0
-	for k,v in pairs(game.players) do
-		if(v.role == "mafia") then
-			aliveMafia = aliveMafia + 1
-		else
-			aliveCitizens = aliveCitizens + 1
-			if(v.role == "doctor") then
-				aliveDoctors = aliveDoctors + 1
-			end
-		end
-	end
-
-	if(aliveMafia == 0) then
-		Server.winGame(game, "citizens")
-	else
-		if(citizens == 0) then
-			Server.winGame(game, "mafia")
-		elseif(citizens == 1) then
-			if(aliveDoctors == 1) then
-				Server.winGame(game, "mafia")
-			end
-		end
-	end
+	Server.checkWin(game)
 
 	--Reset
 	game.currentRound = currentRound + 1
@@ -245,18 +277,24 @@ function Server.endRound(game)
 	Server.saveGames()
 end
 
+--Begins the voting period
 function Server.beginVoting(game)
-	game.currentPhase = "voting"
-	game.timeRemaining = game.settings.votingMinutes
-	Server.saveGames()
+	if(Server.checkWin(game) == false) then
+		game.currentPhase = "voting"
+		game.timeRemaining = game.settings.votingMinutes
+		Server.saveGames()
+	end
 end
 
+
+--Begins the active period
 function Server.beginActive(game)
 	game.currentPhase = "active"
 	game.timeRemaining = game.settings.activeMinutes
 	Server.saveGames()
 end
 
+--Represents a single game tick
 function gameTick(game)
 	local round = game.currentRound
 	if(round == "lobby") then
@@ -277,6 +315,7 @@ function gameTick(game)
 	Server.saveGames()
 end
 
+--Handles a player's vote
 function Server.vote(token, targetName)
 	if(Server.validate(token)) then
 		local username = Server.tokens[token].username
@@ -294,6 +333,8 @@ function Server.vote(token, targetName)
 	end
 end
 
+--Adds a user to and existing game
+--Returns false if unable to add user
 function Server.joinGame(token, gameCode)
 	local username = Server.tokens[token].username
 	local game = Server.games[gameCode]
@@ -317,6 +358,7 @@ function Server.joinGame(token, gameCode)
 	return false
 end
 
+--Assigns roles and starts the game tick cycle
 function Server.startGame(token)
 	local username = Server.tokens[token].username
 	local game = Server.games[Server.accounts[username].currentGame]
@@ -354,6 +396,7 @@ function Server.startGame(token)
 	return false
 end
 
+--Removes a player from the current game
 function Server.leaveGame(token)
 	local username = Server.tokens[token].username
 	local currentGame = Server.accounts[username].currentGame
@@ -363,6 +406,7 @@ function Server.leaveGame(token)
 	Server.saveAccount()
 end
 
+--Gets the remaining time in the current phase
 function Server.getRemainingTime(token)
 	if(Server.validate(token)) then
 		local game = Server.games[Server.accounts[Server.tokens[token].username].currentGame]
@@ -370,6 +414,8 @@ function Server.getRemainingTime(token)
 	end
 end
 
+--Handles the use of a player's special ability
+--Returns the role of the target if player is a doctor
 function Server.useSpecial(token, targetName)
 	if(Server.validate(token)) then
 		local username = Server.tokens[token].username
@@ -391,19 +437,12 @@ function Server.useSpecial(token, targetName)
 	return nil
 end
 
+--Handles a potential player death
 function Server.playerDied(token)
 	if(Server.validate(token)) then
 		local username = Server.tokens[token].username
 		Server.games[Server.accounts[username].currentGame].deaths[username] = true
 		Server.saveGames()
-	end
-end
-
-function Server.getPlayerRole(token)
-	if(Server.validate(token)) then
-		local username = Server.tokens[token].username
-		local game = Server.games[Server.accounts[username].currentGame]
-		return game.player[username].role
 	end
 end
 
