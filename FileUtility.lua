e9760ddf0f3550fb0590984bce2dc7c5f7f8bbb0
Tristan Jay <capdincrando@gmail.com>
2019-12-01 16:59:14 -0600

    Finished FileUtility documentation

--- FileUtility.lua
+++ FileUtility.lua
@@ -1,9 +1,22 @@
+--Project Name: Extreme Mafia
+--Authors: Tristan Jay, Max Stevenson, Jesse Wood
+--Class: CS 371
+--Instructor: Dr. Haeyong Chung
+--Due Date: December 1, 2019
+--File Name: FileUtility.lua
+--Description:	Represents the player's account. Used as an interface
+--				with the server and stores account information.
+
+--Libraries
 local json = require( "json" )
 
+--Create FileUtility object
 local FileUtility = {}
 
+--Default directory for the files
 local directory = system.DocumentsDirectory
 
+--Saves a table into the specified file
 function FileUtility.saveTable(t, filename)
     -- Path for the file to write
     local path = system.pathForFile( filename, directory )
@@ -24,6 +37,7 @@ function FileUtility.saveTable(t, filename)
     end
 end
 
+--Creates a table using the data from the given file
 function FileUtility.loadTable(filename)
     -- Path for the file to read
     local path = system.pathForFile( filename, directory )
