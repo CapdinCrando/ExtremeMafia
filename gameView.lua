41a26f2af628b1f5305288f5d6ecbe49bd5a7be3
programmerwood <57074769+programmerwood@users.noreply.github.com>
2019-12-01 23:11:18 -0600

    Update gameView.lua

--- gameView.lua
+++ gameView.lua
@@ -31,103 +31,125 @@ function gameView:create( event )
 	})
 	sceneGroup:insert(accountButton)
 	
-	-- Table Functions
-	local function onRowRender(event)
-		-- event.row
-		local row = event.row
-		local rowHeight = row.contentHeight
-		local rowWidth = row.contentWidth
-		
-		-- Username text
-		local userText = display.newText(row, event.row.params.user, 0, 0, nil, 20)
-		userText:setFillColor(245/255, 245/255, 245/255)
-		userText.anchorX = 0
-		userText.x = 15
-		userText.y = rowHeight * 0.5
-		
-		-- If game phase is voting --------------------------------------------
-		-- Vote button (display as needed)
-		function voteButtonHandler(event)
-			return true
-		end
-		local voteButton = widget.newButton({
-			onEvent = voteButtonHandler,
-			width = 50,
-			height = 25,
-			defaultFile = "vote_default.png",
-			overFile = "vote_over.png",
-		})
-		row:insert(voteButton)
-		voteButton.anchorX = 0
-		voteButton.x = rowWidth - voteButton.width - 15
-		voteButton.y = rowHeight * 0.5
-		
-		-- Vote count (display as needed)
-		if (event.row.params.votes ~= nil) then
-			local voteCount = display.newText(row, event.row.params.votes, 0, 0, nil, 20)
-			voteCount:setFillColor(245/255, 245/255, 245/255)
-			voteCount.anchorX = 0
-			voteCount.x = voteButton.x - 15
-			voteCount.y = rowHeight * 0.5
-		end
-		
-		-- If game phase is active --------------------------------------------
-		-- Alive or dead state (currently tries to use a nil value, commented out
-		--[[
-		local playerState = display.newText(row, event.row.params.state, 0, 0, nil, 15)
-		playerState.anchorX = 0
-		playerState.x = rowWidth - playerState.width - 15
-		playerState.y = rowHeight * 0.5
-		]]
-		
-	end
-	local function onRowUpdate(event)
-		-- event.row
-		-- event.row.index
-		print("update")
-	end
-	local function onRowTouch(event)
-		-- event.phase == "press"
-		-- event.target.index == row touced
-		print("touch")
-	end
-	local function gameTableListener(event)
-		return true
-	end
-	
-	-- Create table
-	local gameTable = widget.newTableView({
-		x = display.contentCenterX,
-		y = display.contentCenterY,
-		width = display.contentWidth - (display.contentWidth/8),
-		height = display.contentHeight - (display.contentHeight/4),
-		listener = gameTableListener,
-		onRowRender = onRowRender,
-		onRowTouch = onRowTouch
-	})
-	
-	-- Populate table
-	local players = Game.getPlayers()
-	for _, item in pairs(players) do
-		gameTable:insertRow({--item.displayName,
-			rowHeight = 50,
-			rowColor = { default = { 0.5, 0.5, 0.5 }, over = { 1, 0, 0 } },
-			params = { user = item.displayName }
-		})
-	end
-	
-	sceneGroup:insert(gameTable)
-	
 end
 
 function gameView:show( event )
  
     local sceneGroup = self.view
     local phase = event.phase
- 
+	
+	--Picture
+	local options = {
+						width = 150,
+						height = 330,
+						numFrames = 4,
+						sheetContentWidth = 625,
+						sheetContentHeight = 330
+					}
+	local sheet = graphics.newImageSheet("MafiaLogo2.png", options)
+	local role = Game.getPlayerRole()
+	local roleIcon
+	if (role == "mafia") then
+		roleIcon = display.newImageRect(sheet, 4, 60, 132)
+	elseif (role == "doctor") then
+		roleIcon = display.newImageRect(sheet, 2, 60, 132)
+	elseif (role == "detective") then
+		roleIcon = display.newImageRect(sheet, 1, 60, 132)
+	else
+		roleIcon = display.newImageRect(sheet, 3, 60, 132)
+	end
+	roleIcon.x = 275
+	roleIcon.y = 20
+	sceneGroup:insert(roleIcon)
+	
     if ( phase == "will" ) then
-        -- Code here runs when the scene is still off screen (but is about to come on screen)
- 
+        -- Table Functions
+		local function onRowRender(event)
+			-- event.row
+			local row = event.row
+			local rowHeight = row.contentHeight
+			local rowWidth = row.contentWidth
+			
+			-- Username text
+			local userText = display.newText(row, event.row.params.user, 0, 0, nil, 20)
+			userText:setFillColor(245/255, 245/255, 245/255)
+			userText.anchorX = 0
+			userText.x = 15
+			userText.y = rowHeight * 0.5
+			
+			-- If game phase is voting --------------------------------------------
+			-- Vote button (display as needed)
+			function voteButtonHandler(event)
+				return true
+			end
+			local voteButton = widget.newButton({
+				onEvent = voteButtonHandler,
+				width = 50,
+				height = 25,
+				defaultFile = "vote_default.png",
+				overFile = "vote_over.png",
+			})
+			row:insert(voteButton)
+			voteButton.anchorX = 0
+			voteButton.x = rowWidth - voteButton.width - 15
+			voteButton.y = rowHeight * 0.5
+			
+			-- Vote count (display as needed)
+			if (event.row.params.votes ~= nil) then
+				local voteCount = display.newText(row, event.row.params.votes, 0, 0, nil, 20)
+				voteCount:setFillColor(245/255, 245/255, 245/255)
+				voteCount.anchorX = 0
+				voteCount.x = voteButton.x - 15
+				voteCount.y = rowHeight * 0.5
+			end
+			
+			-- If game phase is active --------------------------------------------
+			-- Alive or dead state (currently tries to use a nil value, commented out
+			--[[
+			local playerState = display.newText(row, event.row.params.state, 0, 0, nil, 15)
+			playerState.anchorX = 0
+			playerState.x = rowWidth - playerState.width - 15
+			playerState.y = rowHeight * 0.5
+			]]
+			
+		end
+		local function onRowUpdate(event)
+			-- event.row
+			-- event.row.index
+			print("update")
+		end
+		local function onRowTouch(event)
+			-- event.phase == "press"
+			-- event.target.index == row touced
+			print("touch")
+		end
+		local function gameTableListener(event)
+			return true
+		end
+		
+		-- Create table
+		local gameTable = widget.newTableView({
+			x = display.contentCenterX,
+			y = display.contentCenterY,
+			width = display.contentWidth - (display.contentWidth/8),
+			height = display.contentHeight - (display.contentHeight/4),
+			listener = gameTableListener,
+			onRowRender = onRowRender,
+			onRowTouch = onRowTouch
+		})
+		
+		-- Populate table
+		local players = Game.getPlayers()
+		for _, item in pairs(players) do
+			gameTable:insertRow({--item.displayName,
+				rowHeight = 50,
+				rowColor = { default = { 0.5, 0.5, 0.5 }, over = { 1, 0, 0 } },
+				params = { user = item.displayName }
+			})
+		end
+		
+		sceneGroup:insert(gameTable)
+	
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
  
